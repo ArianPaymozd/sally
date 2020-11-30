54d8ef19135042be27704295b1dae54d793750e6
ArianPaymozd <arianyazdi@yahoo.com>
2020-11-30 13:01:15 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+<main> 
+<header>
+<img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="jenny"/>
+</header>
+<h1 className="title">About me</h1>
+  <section id="introduction">
+        <p>Hi I'm Sally I'm 25 and below this line you'll find more information about me.</p>
+    </section>
+    <br />
+    <section id="Education">
+        <p>I have studied at:</p>
+        <ul>
+            <li>IES BEZMILIANA</li>
+            <li>Minnesota University</li>
+        </ul>
+    </section>
+    <br />
+    <section id="works">
+        <p>I have worked at:</p>
+        <div id="works_list">
+            <ul>
+                <li>Github as Engineer during 2018 and there I have to
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </li>
+                <li>Jetbrains as Project manager during 2019 and I'm still working there. My work consist in
+                    A assumenda autem dolorem doloremque
+                </li>
+            </ul>
+        </div>
+    </section>
+    <br />
+    <section id="Contact-section">
+        <p>My contact information: </p>
+        <address id="Contact-info">
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="jenny"/>
+            <p>My name is Sally</p>
+            <p>My email is: <a href="mailto://sally@IntenetExplorer.old">sally@IntenetExplorer.old</a></p>
+            <p>My phone number is: <a href="tel:+34100572415">+34 100 57 24 15</a></p>
+        </address>
+    </section>
+    </main>
+  )
 }
 
 export default App;
